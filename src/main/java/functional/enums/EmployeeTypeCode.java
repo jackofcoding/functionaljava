f07c6ba87254e2891fd7601b72ddf8bbcfb79397
Joji Joy <jojijoy1512@gmail.com>
2023-08-16 14:28:59 +0530

    Code formatting

--- src/main/java/functional/enums/EmployeeTypeCode.java
+++ src/main/java/functional/enums/EmployeeTypeCode.java
@@ -1,2 +1,26 @@
-package functional.model;public enum EmployeeType {
+package functional.enums;
+
+public enum EmployeeTypeCode {
+
+  C_SUITE("CSE", 1), DIRECTOR("DIR", 3),
+  ANALYST("AYT", 9), DEVELOPER("DEV", 7),
+  SUBJECT_MATTER_EXPERT("SME", 6), TESTER("QAT", 8),
+  SCRUM_MASTER("SM", 11), RELEASE_TRAIN_ENGINEER("RTE", 4);
+
+  private final String employeeCode;
+  private final Integer rank;
+
+  EmployeeTypeCode(String employeeCode, Integer rank) {
+    this.employeeCode = employeeCode;
+    this.rank = rank;
+  }
+
+  public String getEmployeeCode() {
+    return this.employeeCode;
+  }
+
+  public Integer getRank() {
+    return this.rank;
+  }
+
 }
