4ac548304f3ba54f1cfb966d16867ad2890d8655
Joji Joy <jojijoy1512@gmail.com>
2023-08-22 10:48:02 +0530

    # Changes
    Code formatting

--- src/test/java/functional/utils/UTF8FileReaderUtilityTest.java
+++ src/test/java/functional/utils/UTF8FileReaderUtilityTest.java
@@ -12,8 +12,8 @@ class UTF8FileReaderUtilityTest {
   void
       test_get_file_contents_as_sentences_from_resources_should_return_file_content_as_sentences_when_file_is_valid() {
     assertThat(
-        UTF8FileReaderUtility.getFileContentsAsSentencesFromResources(
-            "src/test/resources", "test_file.txt"))
+            UTF8FileReaderUtility.getFileContentsAsSentencesFromResources(
+                "src/test/resources", "test_file.txt"))
         .asList()
         .hasSize(2)
         .hasSameElementsAs(List.of("This is a test file", "This contains sentences in one line"));
@@ -23,10 +23,9 @@ class UTF8FileReaderUtilityTest {
   void
       test_get_file_contents_as_sentences_from_resources_should_not_throw_exception_when_file_is_invalid() {
     assertThatCode(
-        () ->
-            UTF8FileReaderUtility.getFileContentsAsSentencesFromResources(
-                "src/test/resources", "invalid.txt"))
+            () ->
+                UTF8FileReaderUtility.getFileContentsAsSentencesFromResources(
+                    "src/test/resources", "invalid.txt"))
         .doesNotThrowAnyException();
   }
-
 }
